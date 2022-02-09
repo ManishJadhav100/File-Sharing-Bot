61a4bee4e28a54b306eb9d8a924036ae1d2a9042
Manish Jadhav <54273848+ManishJadhav100@users.noreply.github.com>
2022-02-09 16:27:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,27 +3,27 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = "o5234474655:AAGKcU8WsbDCKFtBCk2DdlvuEUPuvRedDqI"
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = 3796974
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = "9511d0112631f9990337eb724d1a7d0d"
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = -1001771465683
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = 1464063686
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = "mongodb+srv://mjwatermark:Manya@007@cluster0.ahbvh.mongodb.net/sample?retryWrites=true&w=majority"
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = -1001234394694
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = 4
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
